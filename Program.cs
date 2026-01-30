590b0a40e5ae18aaeaf559ab84907fa8ad93d3af
smartgirl11-10 <prabhsehmi97@gmail.com>
2026-01-30 09:54:31 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StudentCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                var student = new Student()
+                {
+                    FirstName = "John",
+                    LastName = "Smith"
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Student added successfully!");
+            Console.ReadLine();
+        }
+    }
+}
